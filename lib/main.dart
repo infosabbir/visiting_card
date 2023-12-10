4e36ad34150626adaec020ad00a6d8bd1450d0a9
infosabbir <infosabbir9@gmail.com>
2023-12-10 23:34:09 +0600

    just created visiting card app

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:v_card/pages/contact_details_page.dart';
+import 'package:v_card/pages/form_page.dart';
+import 'package:v_card/pages/home_page.dart';
+import 'package:v_card/pages/scan_page.dart';
+import 'package:v_card/provider/contact_provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ContactProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  static const String routeName = '/';
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: HomePage.routeName,
+      routes: {
+        HomePage.routeName: (context) => const HomePage(),
+        ScanPage.routeName: (context) => const ScanPage(),
+        FormPage.routeName: (context) => const FormPage(),
+        ContactDetailsPage.routeName: (context) => const ContactDetailsPage(),
+      },
+    );
+  }
+}
---
+++ lib/pages/contact_details_page.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class ContactDetailsPage extends StatefulWidget {
+  static const String routeName = '/details';
+  const ContactDetailsPage({super.key});
+
+  @override
+  State<ContactDetailsPage> createState() => _ContactDetailsPageState();
+}
+
+class _ContactDetailsPageState extends State<ContactDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
